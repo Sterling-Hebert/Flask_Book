4eb889fed4afa6fe6dc3f397c96181462faada90
sterling_hebert <sterling_hebert@outlook.com>
2023-05-04 16:37:38 -0500

    added visits counter

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session
 
 from .config import Config
 
@@ -9,4 +9,22 @@ app.config.from_object(Config)
 
 @app.route("/")
 def index():
+    visits()
     return "Tweeter"
+
+
+@app.route('/visits-counter/')
+def visits():
+    if 'visits' in session:
+        # reading and updating session data
+        session['visits'] = session.get('visits') + 1
+    else:
+        # setting session data
+        session['visits'] = 1
+    return "Total visits: {}".format(session.get('visits'))
+
+
+@app.route('/delete-visits/', methods=["POST"])
+def delete_visits():
+    session.pop('visits', None)  # delete visits
+    return 'Visits deleted'
